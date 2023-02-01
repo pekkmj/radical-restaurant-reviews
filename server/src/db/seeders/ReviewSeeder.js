f2ac8182111f60e3054a39fe136e853a4baaad91
Vincent Kwan <vkk@Vincents-MacBook-Pro.local>
2023-02-01 17:09:22 -0500

    trying spaces:2 in NewRestaurantForm.js

--- server/src/db/seeders/ReviewSeeder.js
+++ server/src/db/seeders/ReviewSeeder.js
@@ -1,23 +1,23 @@
 import { Restaurant, Review } from "../../models/index.js";
 
 class ReviewSeeder {
-    static async seed() {
-        const chuckECheese = await Restaurant.query().findById(1)
-        const chuckECheeseReviewData = [
-            {
-                subject: "Best restaurant ever",
-                body: "adsdsa",
-                rating: 5
-            }
-        ]
+  static async seed() {
+    const chuckECheese = await Restaurant.query().findById(1)
+    const chuckECheeseReviewData = [
+      {
+        subject: "Best restaurant ever",
+        body: "adsdsa",
+        rating: 5
+      }
+    ]
 
-        for (const singleReviewData of chuckECheeseReviewData){
-            const currentReview = await Review.query().findOne(singleReviewData)
-            if (!currentReview) {
-                await chuckECheese.$relatedQuery("reviews").insert(singleReviewData)
-            }
-        }
+    for (const singleReviewData of chuckECheeseReviewData) {
+      const currentReview = await Review.query().findOne(singleReviewData)
+      if (!currentReview) {
+        await chuckECheese.$relatedQuery("reviews").insert(singleReviewData)
+      }
     }
+  }
 }
 
 export default ReviewSeeder
--- server/src/models/Review.js
+++ server/src/models/Review.js
@@ -1,36 +1,36 @@
 const Model = require("./Model.js");
 
 class Review extends Model {
-    static get tableName() {
-        return "reviews"
-    }
+	static get tableName() {
+		return "reviews"
+	}
 
-    static get jsonSchema () {
-        return {
-            type: "object",
-            required: ["subject", "rating"],
-            properties: {
-                subject: { type: "string" },
-                body: { type: "string" },
-                rating: { type: ["integer", "string"] }
-            }
-        }
-    }
+	static get jsonSchema() {
+		return {
+			type: "object",
+			required: ["subject", "rating"],
+			properties: {
+				subject: { type: "string" },
+				body: { type: "string" },
+				rating: { type: ["integer", "string"] }
+			}
+		}
+	}
 
-    static get relationMappings() {
-        const { Restaurant } = require("./index.js")
+	static get relationMappings() {
+		const { Restaurant } = require("./index.js")
 
-        return {
-            restaurant: {
-                relation: Model.BelongsToOneRelation,
-                modelClass: Restaurant,
-                join: {
-                    from: "reviews.restaurantId",
-                    to: "restaurants.id"
-                }
-            }
-        }
-    }
+		return {
+			restaurant: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Restaurant,
+				join: {
+					from: "reviews.restaurantId",
+					to: "restaurants.id"
+				}
+			}
+		}
+	}
 }
 
 module.exports = Review
