85eae165c8dff3c793412a53558812c7c1057438
Vincent Kwan <vkk@Vincents-MacBook-Pro.local>
2023-02-01 14:05:17 -0500

    moved states array out of form into constants folder

--- client/src/components/NewRestaurantForm.js
+++ client/src/components/NewRestaurantForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import states from "./constants/states.js"
 
 const NewRestaurantForm = ({ addRestaurant }) => {
     const emptyNewRestaurant = {
@@ -10,68 +11,6 @@ const NewRestaurantForm = ({ addRestaurant }) => {
         description: ''
     }
     const [newRestaurant, setNewRestaurant] = useState(emptyNewRestaurant)
-    const states = [
-        '',
-        'Alabama',
-        'Alaska',
-        'American Samoa',
-        'Arizona',
-        'Arkansas',
-        'California',
-        'Colorado',
-        'Connecticut',
-        'Delaware',
-        'District of Columbia',
-        'Federated States of Micronesia',
-        'Florida',
-        'Georgia',
-        'Guam',
-        'Hawaii',
-        'Idaho',
-        'Illinois',
-        'Indiana',
-        'Iowa',
-        'Kansas',
-        'Kentucky',
-        'Louisiana',
-        'Maine',
-        'Marshall Islands',
-        'Maryland',
-        'Massachusetts',
-        'Michigan',
-        'Minnesota',
-        'Mississippi',
-        'Missouri',
-        'Montana',
-        'Nebraska',
-        'Nevada',
-        'New Hampshire',
-        'New Jersey',
-        'New Mexico',
-        'New York',
-        'North Carolina',
-        'North Dakota',
-        'Northern Mariana Islands',
-        'Ohio',
-        'Oklahoma',
-        'Oregon',
-        'Palau',
-        'Pennsylvania',
-        'Puerto Rico',
-        'Rhode Island',
-        'South Carolina',
-        'South Dakota',
-        'Tennessee',
-        'Texas',
-        'Utah',
-        'Vermont',
-        'Virgin Island',
-        'Virginia',
-        'Washington',
-        'West Virginia',
-        'Wisconsin',
-        'Wyoming'
-    ]
 
     const handleInputChange = (event) => {
         setNewRestaurant({
