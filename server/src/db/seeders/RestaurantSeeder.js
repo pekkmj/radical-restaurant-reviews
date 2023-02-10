73d5f6bef36651a605bcc99c333aae8d4ac0599b
Vincent Kwan <vkk@Vincents-MacBook-Pro.local>
2023-02-10 12:45:19 -0500

    add userID

--- server/src/db/seeders/RestaurantSeeder.js
+++ server/src/db/seeders/RestaurantSeeder.js
@@ -10,6 +10,38 @@ class RestaurantSeeder {
         state: "Massachusetts",
         zipCode: "02149",
         description: "Happiest place on Earth!"
+      },
+      {
+        name: "Five Guys Burgers and Fries",
+        address: "58 Summer St",
+        city: "Boston",
+        state: "Massachusetts",
+        zipCode: "02110",
+        description: ""
+      },
+      {
+        name: "Ichiban",
+        address: "462 Quincy Ave",
+        city: "Braintree",
+        state: "Massachusetts",
+        zipCode: "02184",
+        description: "Hotpot and sushi"
+      },
+      {
+        name: "Anna's Taqueria",
+        address: "242 Cambridge St",
+        city: "Boston",
+        state: "Massachusetts",
+        zipCode: "02114",
+        description: "Mexican fine dining"
+      },
+      {
+        name: "Chili's",
+        address: "1040 Revere Beach Pkwy",
+        city: "Chelsea",
+        state: "Massachusetts",
+        zipCode: "02150",
+        description: "Has everything"
       }
     ]
 
--- server/src/db/seeders/ReviewSeeder.js
+++ server/src/db/seeders/ReviewSeeder.js
@@ -2,20 +2,40 @@ import { Restaurant, Review } from "../../models/index.js";
 
 class ReviewSeeder {
   static async seed() {
-    const chuckECheese = await Restaurant.query().findById(1)
-    const chuckECheeseReviewData = [
-      {
-        userId: 1,
-        subject: "Best restaurant ever",
-        body: "Tasty food, good entertainment",
-        rating: 5
-      }
-    ]
+    const reviewsData = {
+      1: [
+        {
+          userId: 1,
+          subject: "Best restaurant ever",
+          body: "Tasty food, good entertainment",
+          rating: 5
+        },
+        {
+          userId: 2,
+          subject: "Scam",
+          body: "I spent $100 and only got 17 tickets",
+          rating: 1
+        }
+      ],
+      2: [
+        {
+          userId: 1,
+          subject: "Expensive",
+          body: "It's good but this place is hella expensive dude",
+          rating: 3
+        }
+      ]
+    }
 
-    for (const singleReviewData of chuckECheeseReviewData) {
-      const currentReview = await Review.query().findOne(singleReviewData)
-      if (!currentReview) {
-        await chuckECheese.$relatedQuery("reviews").insert(singleReviewData)
+    let restaurant
+    for (let restaurantId in reviewsData) {
+      restaurant = await Restaurant.query().findById(restaurantId)
+
+      for (const singleReviewData of reviewsData[restaurantId]) {
+        const currentReview = await Review.query().findOne(singleReviewData)
+        if (!currentReview) {
+          await restaurant.$relatedQuery("reviews").insert(singleReviewData)
+        }
       }
     }
   }
